9dfb5967e3acde981518612b418c628db66d619b
LucasSilvaIT <lucas.silva.it@outlook.com>
2025-05-22 10:02:41 +0100

    test: router monitoring service test added

---
+++ network-service/src/test/kotlin/com/github/project/networkservice/services/RouterMonitoringServiceTest.kt
@@ -0,0 +1,65 @@
+package com.github.project.networkservice.services
+
+import com.github.project.api.PluginLoader
+import com.github.project.api.router.RouterMonitoring
+import com.github.project.networkservice.exceptions.RouterNotFoundException
+import com.github.project.networkservice.models.Router
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.`when`
+import org.mockito.junit.jupiter.MockitoExtension
+
+@ExtendWith(MockitoExtension::class)
+class RouterMonitoringServiceTest {
+
+    @Mock
+    private lateinit var routerService: RouterService
+
+    @Mock
+    private lateinit var pluginLoader: PluginLoader
+
+    @Mock
+    private lateinit var routerMonitoring: RouterMonitoring
+
+    @InjectMocks
+    private lateinit var routerMonitoringService: RouterMonitoringService
+
+    @Test
+    fun `get total memory should succeed`() {
+
+        val router = Router(id = 1, vendor = "Mikrotik", ipAddress = "192.168.0.2", model = "Router OS")
+
+        `when`(routerService.getById(1)).thenReturn(router)
+        `when`(
+            pluginLoader.getRouterMonitoring(
+                model = router.model.lowercase(),
+                hostname = router.ipAddress,
+                port = 161
+            )
+        ).thenReturn(routerMonitoring)
+        `when`(routerMonitoring.getTotalMemory())
+            .thenReturn(1000)
+
+        routerMonitoringService.getTotalMemory(router.id)
+
+        verify(routerMonitoring).getTotalMemory()
+    }
+
+    @Test
+    fun `get up time from a router that does not exist shouldn't succeed`() {
+
+        val router = Router(id = 1, vendor = "Mikrotik", ipAddress = "192.168.0.2", model = "Router OS")
+
+        `when`(routerService.getById(1)).thenThrow(RouterNotFoundException::class.java)
+
+        assertThrows<RouterNotFoundException> {
+            routerMonitoringService.getUptime(router.id)
+        }
+    }
+
+
+}
